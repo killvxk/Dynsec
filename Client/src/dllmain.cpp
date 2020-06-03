6c4cfcbb974f5a420b0116729d5008fa7af530fc
Cameron <25990062+xCameronx@users.noreply.github.com>
2020-06-03 21:45:33 +0100

    testing grounds

--- Client/src/dllmain.cpp
+++ Client/src/dllmain.cpp
@@ -8,13 +8,7 @@ __declspec(dllexport) void InitializeClient(void* pDynsecData) {
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
     if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
-#if _DEBUG
-        FILE* stream;
-        AllocConsole();
-        freopen_s(&stream, "CONIN$", "w", stdin);
-        freopen_s(&stream, "CONOUT$", "w", stdout);
-        freopen_s(&stream, "CONOUT$", "w", stderr);
-#endif
+        // init
     }
 
     return TRUE;
---
+++ TestingGrounds/src/TestingGrounds.cpp
@@ -0,0 +1,24 @@
+#include <Windows.h>
+#include <iostream>
+
+int main() {
+    struct Callbacks {
+
+    };
+
+    HMODULE Dynsec = LoadLibraryA("Client.dll");
+    if (Dynsec) {
+        printf("Dynsec client: %llx\n", Dynsec);
+        FARPROC Initialize = GetProcAddress(Dynsec, "?InitializeClient@@YAXPEAX@Z");
+        if (Initialize) {
+            printf("InitializeClient: %llx\n", Initialize);
+            Callbacks* Data = new Callbacks();
+            ((void(*)(Callbacks*))Initialize)(Data);
+
+            // temp
+            delete Data;
+        }
+    }
+
+    system("pause");
+}
